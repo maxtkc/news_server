3092e6fe3a9aa7bd58cbec9c8b33b690ae9381e9
Sam Christy <schristy@medford.k12.ma.us>
2020-06-16 10:55:20 -0400

    Show problem parallel printing

--- online_news_server.py
+++ online_news_server.py
@@ -7,19 +7,26 @@ import time
 
 app = Flask(__name__)
 
+# Establish serial connection and flush any previous serial data
+# Note: Should make port selection in browser
 ser = serial.Serial('/dev/ttyUSB0', 57600)
 ser.flush()
 
+# Hardcoded URL for news server
 #url = 'https://newsapi.org/v2/top-headlines?'
 url = 'https://newsapi.org/v2/everything?'
 
-tdays = 18
+# Hardcoded sort selection
+# Note: Need to add this to selection on server
 sort = 'popularity'
 
+# Initial page view
+# Note: Do I need two page views?
 @app.route('/')
 def index():
 	return render_template('news.html')
 
+# Gather form entries from news page. 
 @app.route('/view', methods = ['GET', 'POST'])
 def set_parameters():
 	if request.method == 'POST':
@@ -35,34 +42,37 @@ def generate_news(search, sources, days):
 		'language': 'en',
 		'apiKey': 'd326d5d1b37b4d6085e89f2c747942f2'
 	}
+	# Create list of news sources
 	source_set = ','.join(sources)
 	
+	# Calculate correct date based on number of past days requested for search
 	dt = datetime.now() -timedelta(days = int(days))
 	month = dt.month
 	day = dt.day
 
+	# Collect all parameters for search request
 	params.update( {'from' : '2020-{}-{}'.format(month, day)} )
 	params.update( {'domains' : '{}'.format(source_set)} )
 	params.update( {'qInTitle' : '{}'.format(search)} )
 	params.update( {'sortBy' : '{}'.format(sort)} )
 
+	# Make request to NewsAPI
 	response = requests.get(url, params=params)
 
+	# Print params and return result of search request as json
 	print(params)
 	return response.json()
 
 def display_news(news):
 
 	for article in news['articles']:
-		print(article['title'])
-
-	for article in news['articles']:
 		title = article['title'].upper()
+		print(title)
 		for letter in title:
-			print(letter, end=" ")
+			print(letter)
 			#ser.write(letter.encode())
-			#time.sleep(.3)
-		ser.write(" ++ ".encode())
+			time.sleep(.3)
+		#ser.write(" ++ ".encode())
 
 def main():
 	news = generate_news()
